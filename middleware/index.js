db2288c5970b5bb6b8c7e79c63da4b4045c9fe9f
Piotr Cytryniak <piotr.cytryniak@gmail.com>
2017-11-16 15:42:50 +0100

    First commit

---
+++ middleware/index.js
@@ -0,0 +1,65 @@
+// Schema definition is also needed here because it is used in functions
+
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// all the middleware goes here
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+    // if you are signed in
+ if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+           if(err){
+               req.flash("error", "Campground not found");
+               res.redirect("back");
+           }  else {
+               // does user own the campground?
+            if(foundCampground.author.id.equals(req.user._id)) {
+                // next() is move on with the codebelow
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that");
+                // redirects to previous page
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+    // if you are signed in
+ if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+           if(err){
+               res.redirect("back");
+           }  else {
+               // does user own the comment?
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    // below flash messages that dissapear after refreshing page. It must be before the redirection
+    req.flash("error", "You need to be logged in to do that");
+    res.redirect("/login");
+}
+
+module.exports = middlewareObj;
